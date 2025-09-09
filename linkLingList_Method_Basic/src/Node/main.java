d4013354a9bd58c2da7f817704c3cb5b0be2ba82
nguyễn minh phúc <110640752+hellomyfriend123@users.noreply.github.com>
2025-09-09 22:52:51 +0700

    Update main.java

--- linkLingList_Method_Basic/src/Node/main.java
+++ linkLingList_Method_Basic/src/Node/main.java
@@ -4,22 +4,30 @@
  */
 package Node;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DELL
  */
 public class main {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         Singlylinklist list = new Singlylinklist();
         
-        list.addFirst(1);
-        list.addTail(2);
-        list.addTail(3);
-        list.addFirst(0);
-        list.addTail(4);
-        list.addTail(5);
+        list.addFirst(5);
+        list.addTail(10);
+        list.addTail(15);
+        list.addTail(20);
+        list.addTail(25);
+        list.addList(2, 7);
         list.PrintLit();
-        System.out.println("Size: " + list.getSize()); 
-    }
+        System.out.println("size list " + list.getSize());
+        System.out.println("Search " + list.search(2));
+        list.removeLast();
+        System.out.println((int) list.search(list.Size()-1));
+        System.out.println("__________");
+        System.out.println(list.search(2));
+        list.removeList(0);
+        System.out.println(list.search(0));
+                }
 }
-
