583e3dcf5b35e91da4aef9076610f12646576448
nguyễn minh phúc <110640752+hellomyfriend123@users.noreply.github.com>
2025-09-09 22:21:31 +0700

    Update Singlylinklist.java

--- linkLingList_Method_Basic/src/Node/Singlylinklist.java
+++ linkLingList_Method_Basic/src/Node/Singlylinklist.java
@@ -51,5 +51,97 @@ public class Singlylinklist {
     public int getSize(){
       return size;
     }
+    
+    public int Size(){
+     Node temp = head;
+     int index = 0;
+     while (temp != null){
+       temp = temp.next;
+       index ++;
+     }
+     return index;
+    }
+    
+    public void addList(int index, int value){
+      Node newNode = new Node(value);
+      
+      if (index == 0){
+          addFirst(value);
+      }
+      
+      else if (index == Size() - 1){
+          addTail(value);
+      }
+      
+      else if (index >= Size()){
+          System.out.println("Invalid index!");
+      }
+      
+      Node temp = head;
+      Node prev = null;
+      
+      int count = 0;
+      
+      while (temp != null && count != index){
+         prev = temp;
+         temp = temp.next;
+         count ++;
+      }
+      newNode.next = temp;
+      prev.next = newNode;
+    }
+    
+    public int search (int index){
+      if (index >= Size() || index < 0){
+         return 0;
+      }
+      
+      int count = 0;
+      Node temp = head;
+      while (temp != null && count != index){
+        temp = temp.next;
+        count ++;
+      }
+      return temp.data;
+    }
+    
+    public void removeFirst(){
+      if (head == null){
+        return;
+      }
+        head.next = head;
+    }
+    
+    public void removeLast(){
+      if (head == null){
+       return ;
+      }
+      Node temp = head;
+      Node prev = null;
+      
+      while (temp != null){
+       prev = temp;
+       temp = temp.next;
+      }
+     
+      prev.next = null;
+    }
+    
+    public void removeList (int index){
+      if (index > Size() || index < 0){
+        return;
+      }
+      Node temp = head;
+      Node prev = null;
+      
+      int count = 0;
+      
+      while (temp != null && count != index){
+        count ++;
+        prev = temp;
+        temp = temp.next;
+      }
+    }
+    
     }
 
